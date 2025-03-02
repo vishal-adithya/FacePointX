fe24d8a781f42289b15d547e2a0079487637b312
Vishal Adithya.A (Windows PC) <vishaladithya2008@gmail.com>
2025-03-03 00:00:00 +0530

    Data loading and preprocessing

--- main.py
+++ main.py
@@ -5,3 +5,25 @@ Created on Sun Mar  2 12:56:36 2025
 @author: vishaladithyaa
 """
 
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+train_csv_filepath = os.path.join("Data","training.csv")
+
+train_df = pd.read_csv(train_csv_filepath)
+
+train_df.fillna(train_df.mean(),inplace = True)
+train_df.dropna(how = "all")
+
+train_df["Image"] = train_df["Image"].apply(lambda x: np.fromstring(x, sep = " ").reshape(96,96,1))
+
+X_imgs = np.stack(train_df["Image"].values) / 255.0
+y = train_df.drop(columns = ["Image"]).values
+
+plt.figure(figsize=(4, 4))
+plt.imshow(X_imgs[1], cmap="gray")
+plt.title("96x96")
+plt.grid(True)
+plt.show()
